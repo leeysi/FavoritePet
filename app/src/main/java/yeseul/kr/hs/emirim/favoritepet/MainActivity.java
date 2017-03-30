71181210456b9725ee3cbbaaca4fa6c0a11d247f
leeysi <yeseul726@e-mirim.hs.kr>
2017-03-30 20:57:15 +0900

    #4 RadioButton Event Programming

--- app/src/main/java/yeseul/kr/hs/emirim/favoritepet/MainActivity.java
+++ app/src/main/java/yeseul/kr/hs/emirim/favoritepet/MainActivity.java
@@ -10,8 +10,9 @@ import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener{
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener, View.OnClickListener{
     CheckBox checkSelect;
     RadioGroup rg;
     RadioButton radioRapmon, radioJin, radioSuga, radioHope, radioJimin, radioV, radioKook;
@@ -35,7 +36,8 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
         butOk = (Button) findViewById(R.id.but_ok);
         imgvMem = (ImageView) findViewById(R.id.imgv_mem);
         checkSelect.setOnCheckedChangeListener(this);
-    }
+        butOk.setOnClickListener(this);
+    }//end onCreate
 
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -49,6 +51,35 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
             rg.setVisibility(View.INVISIBLE);
             butOk.setVisibility(View.INVISIBLE);
             imgvMem.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(rg.getCheckedRadioButtonId()){
+            case R.id.radio_rapmon:
+                imgvMem.setImageResource(R.drawable.rapmonster);
+                break;
+            case R.id.radio_jin:
+                imgvMem.setImageResource(R.drawable.jin);
+                break;
+            case R.id.radio_suga:
+                imgvMem.setImageResource(R.drawable.suga);
+                break;
+            case R.id.radio_hope:
+                imgvMem.setImageResource(R.drawable.jhope);
+                break;
+            case R.id.radio_jimin:
+                imgvMem.setImageResource(R.drawable.jimin);
+                break;
+            case R.id.radio_V:
+                imgvMem.setImageResource(R.drawable.taehyung);
+                break;
+            case R.id.radio_kook:
+                imgvMem.setImageResource(R.drawable.jungkook);
+                break;
+            default:
+                Toast.makeText(this, "라디오버튼이 선택되지 않았습니다.", Toast.LENGTH_SHORT).show();
         }
     }
 }
